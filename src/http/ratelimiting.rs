c31a4081e3c95ab744f8ef55f7b1f1c6ed422ead
kpyzhyanov <an1ko@yahoo.com>
2022-01-10 17:56:43 +0100

    Adding debug for request

--- src/http/ratelimiting.rs
+++ src/http/ratelimiting.rs
@@ -190,6 +190,7 @@ impl Ratelimiter {
             bucket.lock().await.pre_hook(&route).await;
 
             let request = req.build(&self.client, &self.token, None, None)?.build()?;
+            println!("Headers:{:?}",&request.headers());
             let response = self.client.execute(request).await?;
 
             // Check if the request got ratelimited by checking for status 429,
